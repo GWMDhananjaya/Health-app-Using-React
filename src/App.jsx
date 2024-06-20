11ee12e0cf781d52f6234b214ba1d020d0330b73
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-06-20 22:09:48 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import Contact from './pages/Contact';
+import Favs from './pages/Favs';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/dentist/:id" element={<Detail />} />
+          <Route path="/contacto" element={<Contact />} />
+          <Route path="/favs" element={<Favs />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
 

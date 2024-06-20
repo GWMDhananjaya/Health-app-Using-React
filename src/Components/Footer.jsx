8fe1a80e015b860333c6fdb8b967106fb0226147
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-06-20 22:05:34 +0530

    start

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import DHlogo from '../../public/images/Dh.ico'
+
+const Footer = () => {
+  return (
+    <footer>
+        {/* <p>Powered by</p>
+        <img src={DHlogo} alt='DH-logo' /> */}
+
+        
+    </footer>
+  )
+}
+
+export default Footer

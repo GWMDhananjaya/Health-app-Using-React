661a040e995d4d9ab1a1a0170ea543782db8a113
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-06-21 15:58:54 +0530

    favs

--- src/Pages/Favs.jsx
+++ src/Pages/Favs.jsx
@@ -17,17 +17,20 @@
 // };
 
 // export default Favs;
-import React, { useContext } from 'react';
-import Card from '../Components/Card';
-import { GlobalContext } from '../Components/utils/global.context';
+import React, { useContext } from "react";
+import Card from "../Components/Card";
+import { GlobalContext } from "../Components/utils/global.context";
 
 const Favs = () => {
   const { state } = useContext(GlobalContext);
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
   return (
-    <div className={`favs-container ${state.theme === 'dark' ? 'dark-theme' : 'light-theme'}`}>
-
+    <div
+      className={`favs-container ${
+        state.theme === "dark" ? "dark-theme" : "light-theme"
+      }`}
+    >
       <h1 className="mb-2 text-3xl font-bold text-center">Dentists Favs</h1>
       <div className="flex flex-wrap justify-around pl-64 pr-64">
         {favorites.length === 0 ? (
@@ -49,4 +52,3 @@ const Favs = () => {
 };
 
 export default Favs;
-
